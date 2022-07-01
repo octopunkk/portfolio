77b7069ab6d0c7c737765a58326384abbb5eed0a
octopunkk <anais.derue@insa-rennes.fr>
2022-07-01 12:29:40 +0200

    add window

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import { Window } from "./Window";
 
 function App() {
   return (
     <div className="App">
-      <h2 class="text_shadows title">Bonjour</h2>
-      <Window />
+      <h2 className="text_shadows title">Bonjour</h2>
+      <div className="window">
+        <div className="top">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </div>
+
+        <div className="content">
+          <h3>Salut</h3>
+          <p>Salut salut salut</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Window.jsx
+++
@@ -1,3 +0,0 @@
-export let Window = (props) => {
-  return <div className="Window"></div>;
-};
